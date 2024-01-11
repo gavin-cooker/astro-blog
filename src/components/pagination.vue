9e864198525cea1ac2cc472c26871d1e9c9579b7
Gavin.lin <gavin.lin@zeasn.com>
2024-01-11 16:25:36 +0800

    feat: 分页改进

---
+++ src/components/pagination.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div>
+            current page: {{ props.currentPage }}; total page: {{ props.maxPage }}
+        </div>
+        <commonLink v-for="number of props.maxPage" class="linkButton" :href="`/blog/${number}`"
+            :class="{ 'active': number === props.currentPage }">
+            <button>{{ number }}</button>
+        </commonLink>
+    </div>
+</template>
+<script lang="ts" setup>
+import commonLink from '../components/commonLink.vue'
+const props = defineProps({
+    currentPage: {
+        type: Number,
+        default: 0
+    },
+    maxPage: {
+        type: Number,
+        default: 0
+    },
+})
+
+</script>
+<style scoped lang="scss">
+button {
+    cursor: pointer;
+}
+.linkButton {
+    margin-right: .5rem;
+}
+.active {
+    button {
+        text-decoration: underline;
+    }
+}
+</style>
