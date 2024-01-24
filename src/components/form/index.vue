dbaf70defb71b518930b0b39c672d046806e5e8c
Gavin.lin <gavin.lin@zeasn.com>
2024-01-24 15:06:27 +0800

    feat: change vue component export

--- src/components/form/index.vue
+++ src/components/form/index.vue
@@ -24,93 +24,115 @@
             <el-input v-model="form.content" type="textarea" />
         </el-form-item>
         <el-form-item>
-            <el-button type="primary" @click="onSubmit">{{ props.type === 'update' ? 'Update' : 'Create' }}</el-button>
+            <el-button type="primary" @click="onSubmit">{{ type === 'update' ? 'Update' : 'Create' }}</el-button>
             <el-button>Cancel</el-button>
         </el-form-item>
     </el-form>
 </template>
-<script lang="ts" setup>
+<script lang="ts">
 import { ElButton, ElForm, ElFormItem, ElInput, ElTag } from 'element-plus'
-import { nextTick, ref, computed } from 'vue';
+import { nextTick, ref, computed, defineComponent } from 'vue';
 const customHeaders = {
     "Content-Type": "application/json",
 }
-const props = defineProps({
-    type: {
-        type: String,
-        default: 'add'
+export default defineComponent({
+    emits: [
+        'onAdd'
+    ],
+    components:{
+        ElButton,
+        ElForm,
+        ElFormItem,
+        ElInput,
+        ElTag,
     },
-    data: {
-        type: Object,
-        default: () => {
-            return {
-                title: '',
-                description: '',
-                author: '',
-                tags: [],
-                content: ''
+    props: {
+        type: {
+            type: String,
+            default: 'add'
+        },
+        data: {
+            type: Object,
+            default: () => {
+                return {
+                    title: '',
+                    description: '',
+                    author: '',
+                    tags: [],
+                    content: ''
+                }
             }
+        }
+    },
+    setup(props, { emit }) {
+        const form = ref(props.data)
+        const { data } = props;
+
+        const inputValue = ref('')
+        const dynamicTags = ref(data.tags)
+        const inputVisible = ref(false)
+        const InputRef = ref<InstanceType<typeof ElInput>>()
+
+        const handleClose = (tag: string) => {
+            dynamicTags.value.splice(dynamicTags.value.indexOf(tag), 1)
+        }
+
+        const showInput = () => {
+            inputVisible.value = true
+            nextTick(() => {
+                InputRef.value!.input!.focus()
+            })
+        }
+
+        const handleInputConfirm = () => {
+            if (inputValue.value) {
+                dynamicTags.value.push(inputValue.value)
+            }
+            inputVisible.value = false
+            inputValue.value = ''
+        }
+        const baseServerUrl = 'http://localhost:8080/api/blog/'
+        const fetchUrl = computed(() => {
+            return props.type === 'add' ? baseServerUrl : baseServerUrl + form.value._id
+        })
+        const fetchBody = computed(() => {
+            return props.type === 'add' ? {
+                method: "PUT",
+                headers: customHeaders,
+                body: JSON.stringify(form.value)
+            } : {
+                method: "POST",
+                headers: customHeaders,
+                body: JSON.stringify(form.value)
+            }
+        })
+        const onSubmit = async () => {
+            console.log('onsubmit gg', form.value);
+            if (!fetchUrl.value) {
+                throw new Error("need valid fetchurl");
+            }
+            try {
+                let response = fetch(fetchUrl.value, fetchBody.value);
+                console.log('response:', response);
+            } catch (e) {
+                console.log('error in on submit');
+            } finally {
+                location.href = '/blog'
+            }
+        }
+        return {
+            form,
+            dynamicTags,
+            handleClose,
+            inputVisible,
+            inputValue,
+            showInput,
+            handleInputConfirm,
+            onSubmit,
         }
     }
 })
-const emits = defineEmits([
-    'onAdd'
-])
-const form = ref(props.data)
-const { data } = props;
 
-const inputValue = ref('')
-const dynamicTags = ref(data.tags)
-const inputVisible = ref(false)
-const InputRef = ref<InstanceType<typeof ElInput>>()
-
-const handleClose = (tag: string) => {
-    dynamicTags.value.splice(dynamicTags.value.indexOf(tag), 1)
-}
-
-const showInput = () => {
-    inputVisible.value = true
-    nextTick(() => {
-        InputRef.value!.input!.focus()
-    })
-}
-
-const handleInputConfirm = () => {
-    if (inputValue.value) {
-        dynamicTags.value.push(inputValue.value)
-    }
-    inputVisible.value = false
-    inputValue.value = ''
-}
-const baseServerUrl = 'http://localhost:8080/api/blog/'
-const fetchUrl = computed(() => {
-    return props.type === 'add' ? baseServerUrl : baseServerUrl + form.value._id
-})
-const fetchBody = computed(() => {
-    return props.type === 'add' ? {
-        method: "PUT",
-        headers: customHeaders,
-        body: JSON.stringify(form.value)
-    } : {
-        method: "POST",
-        headers: customHeaders,
-        body: JSON.stringify(form.value)
-    }
-})
-const onSubmit = async () => {
-    console.log('onsubmit gg', form.value);
-    if (!fetchUrl.value) {
-        throw new Error("need valid fetchurl");
-    }
-    try {
-        let response = fetch(fetchUrl.value, fetchBody.value);
-        console.log('response:', response);
-    } catch (e) {
-        console.log('error in on submit');
-    } finally {
-        location.href = '/blog'
-    }
-}
 </script>
 <style>
 .button {
